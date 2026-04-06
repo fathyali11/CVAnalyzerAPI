4fbb2492cff9b4d51a9c58c422931155b1f4d72c
fathy <fathy@FATHYALI>
2026-04-06 14:06:29 +0200

    add models

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CVAnalyzerAPI.Models;
+
+public class ApplicationUser:IdentityUser
+{
+    public DateTime CreatedAt { get; set; }
+
+    public ICollection<CV> CVs { get; set; } = [];
+}
